021f827a4f41c9696f975788db9a2c65025d7b38
Coatrieux Anton <anton49110@gmail.com>
2024-10-22 13:02:39 +0200

    chapitre 8

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
@@ -20,9 +19,9 @@ class MainController extends AbstractController
             'status' => 'under construction',
         ];
 
-        return $this->render('main/homepage.html.twig',[
+        return $this->render('main/homepage.html.twig', [
             'numberOfStarships' => $starshipCount,
-            'myShip'=> $myShip,
+            'myShip' => $myShip,
         ]);
     }
 }
